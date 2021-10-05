bf6690b5de62c536c53a54de2b8b9560b5cec01b
Nick Patrikeos <n.patrikeos@unsw.edu.au>
2021-10-05 22:05:45 +1100

    Include q3 in answers

--- tute04/solutions/src/stream/App.java
+++ tute04/solutions/src/stream/App.java
@@ -11,6 +11,7 @@ public class App {
         strings.forEach(s -> System.out.println(s));
 
         List<String> strings2 = new ArrayList<String>(Arrays.asList(new String[] {"1", "2", "3", "4", "5"}));
+        // Q3 List<Integer> ints = strings2.stream().map(i -> Integer.parseInt(i)).collect(Collectors.toList()); 
         List<Integer> ints = strings2.stream().map(Integer::parseInt).collect(Collectors.toList()); 
         System.out.println(ints);
     }
