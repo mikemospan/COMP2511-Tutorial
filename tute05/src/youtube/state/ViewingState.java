7a26ef0b6fa4e669a4edd2b3e3422cc94af13313
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2021-10-10 11:56:47 +1100

    Tute 05

---
+++ tute05/src/youtube/state/ViewingState.java
@@ -0,0 +1,23 @@
+package youtube.state;
+
+import youtube.Viewing;
+
+public abstract class ViewingState {
+
+    private Viewing viewing;
+
+    public ViewingState(Viewing viewing) {
+        this.viewing = viewing;
+    }
+
+    public abstract String onLock();
+
+    public abstract String onPlay();
+
+    public abstract String onNext();
+
+    public Viewing getViewing() {
+        return viewing;
+    }
+
+}
